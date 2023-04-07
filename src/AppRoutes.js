20fb8727b84444f48f1c3baa3b558c89a2aab4a0
Ghimirevivek <stephengmre@gmail.com>
2023-04-07 18:41:09 +0530

    meee

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { Index } from './pages/Index'
-import { NotFound } from './pages/NotFound'
+import Home from './pages/Home'
+import Index from './pages/Index'
+import NotFound from './pages/NotFound'
 
 export const AppRoutes = () => {
   return (
